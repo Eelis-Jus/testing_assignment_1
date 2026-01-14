9b85ff3c6c2b004e04472f235b38de5f891ca133
Eelis-Jus <t3juee01@students.oamk.fi>
2026-01-15 01:10:33 +0200

    -

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,23 @@
+import { divide } from "../src/calculator.js";
+
+describe('divide', ()=>{
+    it('divides 6 by 2', ()=>{
+        expect(divide(6,2)).toBe(3);
+    });
+
+    it('throws when a is not number', ()=>{
+        expect(()=>divide('6',2)).toThrow('Both arguments must be numbers');
+    });
+
+    it('throws when b is not number', ()=>{
+        expect(()=>divide(6,'2')).toThrow('Both arguments must be numbers');
+    });
+
+    it('throws when a is NaN', ()=>{
+        expect(()=>divide(NaN,2)).toThrow('Arguments cannot be NaN');
+    });
+
+    it('throws when a is NaN', ()=>{
+        expect(()=>divide(6,NaN)).toThrow('Arguments cannot be NaN');
+    });
+})
